ddff0973e9041fe06e822a4af40a7c42d344e98c
cao đức tín <102477829+caoductin@users.noreply.github.com>
2024-09-28 23:31:59 +0700

    create post page  and  display userComment

---
+++ AppComic/UICommon/UserCommentCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserCommentCell.swift
+//  AppComic
+//
+//  Created by cao duc tin  on 28/9/24.
+//
+
+import SwiftUI
+
+struct UserCommentCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserCommentCell()
+}
---
+++ AppComic/View/HeadlineDisplay.swift
@@ -0,0 +1,18 @@
+//
+//  HeadlineDisplay.swift
+//  AppComic
+//
+//  Created by cao duc tin  on 28/9/24.
+//
+
+import SwiftUI
+
+struct HeadlineDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeadlineDisplay()
+}
