817cd46f1cdd66e6e2f6738895c01918b806c56c
cao đức tín <102477829+caoductin@users.noreply.github.com>
2024-10-02 22:26:53 +0700

    use swiftData to store postData

---
+++ AppComic/View/testView.swift
@@ -0,0 +1,18 @@
+//
+//  testView.swift
+//  AppComic
+//
+//  Created by cao duc tin  on 30/9/24.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    testView()
+}
