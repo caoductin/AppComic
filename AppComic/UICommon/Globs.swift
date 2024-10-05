7cb9b7a589f0fdb68abf3c34c373b74e05cf41dc
cao đức tín <102477829+caoductin@users.noreply.github.com>
2024-10-05 22:02:05 +0700

    fix the SignUp function and signupView

---
+++ AppComic/UICommon/Globs.swift
@@ -0,0 +1,18 @@
+//
+//  Globs.swift
+//  AppComic
+//
+//  Created by cao duc tin  on 5/10/24.
+//
+
+import SwiftUI
+
+struct Globs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Globs()
+}
