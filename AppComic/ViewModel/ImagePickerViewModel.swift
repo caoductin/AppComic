8ae8ba3353044cf9805233d5164cd8a9d6e10596
cao đức tín <102477829+caoductin@users.noreply.github.com>
2024-10-10 21:53:05 +0700

    store the image to firebase

---
+++ AppComic/ViewModel/ImagePickerViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerViewModel.swift
+//  AppComic
+//
+//  Created by cao duc tin  on 10/10/24.
+//
+
+import SwiftUI
+
+struct ImagePickerViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerViewModel()
+}
---
+++ AppComic/ViewModel/RichTextEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  RichTextEditorView.swift
+//  AppComic
+//
+//  Created by cao duc tin  on 10/10/24.
+//
+
+import SwiftUI
+
+struct RichTextEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RichTextEditorView()
+}
